6d1e7f7fddc9847c3a83c541f47a8de95a56a416
yura yura <stonefiz@gmail.com>
2023-09-04 00:26:05 +0300

    modified odd.py

--- chapter_1/odd.py
+++ chapter_1/odd.py
@@ -1,3 +1,5 @@
+import random
+import time
 from datetime import datetime
 
 odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
@@ -11,3 +13,5 @@ for i in range(5):
         print("This minute seems a little odd.")
     else:
         print("Not an odd minute")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
